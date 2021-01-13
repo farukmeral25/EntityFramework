3658877ceff02c5eb1df64262c7e2e5062faa579
farukmeral25 <farukmeral25@gmail.com>
2021-01-13 19:30:24 +0300

    C#-Framework

---
+++ 20200618_EntityFramework/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20200618_EntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindDataContext ctx = new NorthwindDataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UrunleriListele();
+        }
+
+        private void UrunleriListele()
+        {
+            //Custom Listeleme
+            //var sonuc = from urun in ctx.Urunlers
+            //            select new
+            //            {
+            //                urun.UrunID,
+            //                urun.UrunAdi,
+            //                urun.Fiyat,
+            //                urun.Stok
+            //            };
+
+
+            var sonuc = from urun in ctx.Urunlers
+                        join kategori in ctx.Kategorilers
+                        on urun.KategoriID equals kategori.KategoriID
+                        join tedarikci in ctx.Tedarikcilers
+                        on urun.TedarikciID equals tedarikci.TedarikciID
+                        select new
+                        {
+                            urun.UrunID,
+                            urun.UrunAdi,
+                            urun.Fiyat,
+                            urun.Stok,
+                            kategori.KategoriAdi,
+                            tedarikci.SirketAdi
+                        };
+
+
+            dataGridView1.DataSource = sonuc;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Urunler urun = new Urunler();
+            urun.UrunAdi = txtUrunAdi.Text;
+            urun.Fiyat = nudFiyat.Value;
+            urun.Stok = Convert.ToInt16(nudStok.Value);
+            ctx.Urunlers.InsertOnSubmit(urun);
+            ctx.SubmitChanges();
+            UrunleriListele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }
+            //seçili satırın id'sini alıyoruz.
+            int id = (int)dataGridView1.CurrentRow.Cells["UrunID"].Value;
+            //Single olsaydı eğer sonuç bulamazsa hata verirdi, oluşabilecek hataları önlemek için singleorDefafult kullanılır.Eğer sonuç bulamazsa geriye null değer dönderir.
+            Urunler urun = ctx.Urunlers.SingleOrDefault(urunId => urunId.UrunID ==  id); 
+            ctx.Urunlers.DeleteOnSubmit(urun);
+            ctx.SubmitChanges();
+            UrunleriListele();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            txtUrunAdi.Text = row.Cells["UrunAdi"].Value.ToString();
+            nudFiyat.Value = Convert.ToDecimal(row.Cells["Fiyat"].Value);
+            nudStok.Value = Convert.ToDecimal(row.Cells["Stok"].Value);
+            txtUrunAdi.Tag = row.Cells["UrunID"].Value;
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = (int)txtUrunAdi.Tag;
+            Urunler urun =  ctx.Urunlers.SingleOrDefault(p => p.UrunID == id);
+            urun.UrunAdi = txtUrunAdi.Text;
+            urun.Fiyat = nudFiyat.Value;
+            urun.Stok = Convert.ToInt16(nudStok.Value);
+            ctx.SubmitChanges();
+            UrunleriListele();
+        }
+    }
+}
---
+++ 20200618_EntityFramework/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20200618_EntityFramework
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        NorthwindDataContext ctx = new NorthwindDataContext();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var sonuc = from urun in ctx.Urunlers
+                        join sd in ctx.SatisDetays
+                        on urun.UrunID equals sd.UrunID
+                        join satis in ctx.Satislars
+                        on sd.SatisID equals satis.SatisID
+                        join musteri in ctx.Musterilers
+                        on satis.MusteriID equals musteri.MusteriID
+                        orderby urun.Fiyat descending //Sıralama
+                        select new
+                        {
+                            urun.UrunAdi,
+                            sd.Adet,
+                            ÜrünFiyat = urun.Fiyat,
+                            SatışFiyat = sd.Fiyat,
+                            satis.SatisTarihi,
+                            musteri.SirketAdi
+                        };
+            dataGridView1.DataSource = sonuc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in ctx.Urunlers
+                        join sd in ctx.SatisDetays
+                        on urun.UrunID equals sd.UrunID
+                        join satis in ctx.Satislars
+                        on sd.SatisID equals satis.SatisID
+                        join musteri in ctx.Musterilers
+                        on satis.MusteriID equals musteri.MusteriID
+                        join kategori in ctx.Kategorilers
+                        on urun.KategoriID equals kategori.KategoriID
+                        group new { sd,kategori} by new { urun.UrunAdi,kategori.KategoriAdi } into grup
+                            let ÜrünAdı = grup.Key.UrunAdi
+                            let KategoriAdı = grup.Key.KategoriAdi
+                            let SatışAdet = grup.Count()
+                            let ToplamSatış = grup.Sum(total => total.sd.Adet * total.sd.Fiyat)
+                            orderby ToplamSatış
+                        select new
+                        {
+                           ÜrünAdı,SatışAdet,ToplamSatış,KategoriAdı
+                        };
+                      
+            dataGridView1.DataSource = sonuc;
+        }
+    }
+}
